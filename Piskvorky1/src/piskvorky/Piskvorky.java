f76dc7a88eb16dbfb6dbdda977af3e6eee8c5a62
Jan Drapela <hdrapela@seznam.cz>
2015-12-30 23:02:49 +0100

    Jeste je treba tyhle veci doladit, pokusim se to nejak zitra udelat (tohle je spis na ukazku). Potrebuju hlavne zkusit, jestli se vse posila tak jak ma .

--- Piskvorky1/src/piskvorky/Piskvorky.java
+++ Piskvorky1/src/piskvorky/Piskvorky.java
@@ -1,19 +1,93 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package piskvorky;
+
+import java.util.*;
 
 /**
  *
- * @author Worker
+ * @author Jan Drapela
  */
 public class Piskvorky {
-
+    static int N = 15; //zatim jsem nastavil velikost desky na 15x15
+    static String[][] Pole = new String[N][N];
     
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args)
+    {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                Pole[i][j] = "";
+            }
+        }
+        nacteHru("Hra1.txt");
+        int Hrac = 1;
+        while (true) {
+            System.out.print("\033[H\033[2J"); // melo by fungovat jako clear screen 
+            System.out.flush();
+            hraciDeska();
+            Prompt(Hrac);
+            if (Hrac == 1) {
+                Hrac = 2;
+            } else {
+                Hrac = 1;
+            }
+        }
     }
-    
+    //public static boolean SaveGame(string aFileName)
+    //{
+
+    //}
+   public static void nacteHru(String aFileName) {
+        Scanner sr = new Scanner(aFileName);
+        String line = "";
+        int i = 0;
+        while (line != null) {
+            line = sr.nextLine();
+            if (line == null) {
+                continue;
+            }
+            if (line == "") {
+                continue;
+            }
+            String[] pom = line.split(" "); // pomocne pole stringu, {"1","0",...}
+            for (int j = 0; j < N; j++) {
+                Pole[i][j] = pom[j];
+            }
+            i++;
+        }
+        sr.close();
+
+    }
+    public static void hraciDeska() {
+        /*  const   */ String H = "                   ";
+        System.out.println();
+        System.out.println();
+        System.out.println(H + "    A B C D E F G H I J K L M N O");
+        System.out.print(H + "   ");
+        for (int j = 0; j < N; j++) {
+            System.out.print(" _");
+        }
+        System.out.println();
+        for (int i = 0; i < N; i++) {
+            String S = Integer.toString(i);
+            if (i < 10) {
+                S = " " + S;
+            }
+            System.out.print(H + S + " |");
+            for (int j = 0; j < N; j++) {
+                S = "_";
+                if (Pole[i][j] != "-") 
+                    S = Pole[i][j];
+                
+                System.out.print(S + "|");
+            }
+            System.out.println();
+        }
+
+    } 
+    static void Prompt(int Hrac) {
+        if (Hrac == 1) {
+            System.out.println(">");
+        } else {
+            System.out.println("                                           >");
+        }
+    }
 }
